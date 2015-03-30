5b34c0940ee2b68923bc0d3df620453c3ded8315
kc584846 <kc584846@sju.edu>
2015-03-30 17:18:42 -0400

    Project2 fixes
    yay!

--- part1/jquery.js
+++ part1/jquery.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
  
  $('.faq_question').click(function() {
+ //answer should appear
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+// if ($(this).parent().hasClass('.open')){
+  //   console.log('opn');
+    //$(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
+    //$(this).closest('.faq').removeClass('open');
  
- if ($(this).parent().is('.open')){
- $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
- $(this).closest('.faq').removeClass('open');
+// }else{
+  //   console.log('close');
+    //var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
+    //$(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
+    //$(this).closest('.faq').addClass('open');
+ //}
  
- }else{
- var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
- $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
- $(this).closest('.faq').addClass('open');
- }
- 
- });
+});
  
 });
     
